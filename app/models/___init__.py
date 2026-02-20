2d166bf1ab7adc27799536ee379c289474fe3413
Selin Irmak <selin.irmak89@gmail.com>
2026-02-21 00:24:44 +0300

    Sync models init changes

--- app/models/___init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from .user import User
-from .user_profile import UserProfile
-
-_all_ = ["Base", "User", "UserProfile"]
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from .base import Base
+
+_all_ = ["Base"]
